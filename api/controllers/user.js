c84a14b9d4c13abab33779a9c4fee3ca2aae8f30
RandyMejiaArias <rdmejia3@espe.edu.ec>
2020-02-12 00:55:24 -0500

    Metodos implementados: -Subir imagen de usuario -Devolver imagen de usuario

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,6 +1,8 @@
 'use strict'
 var bcrypt = require('bcrypt-nodejs');
 var mongoosePaginate = require('mongoose-pagination');
+var fs = require('fs');
+var path = require('path');
 var User = require('../models/user');
 var jwt = require('../services/jwt');
 
@@ -123,7 +125,54 @@ function updateUser(req, res){
         if(err) return res.status(500).send({ message: 'Error en la petición.'});
         if(!userUpdated) return res.status(404).send({ message: 'No se ha podido actualizar el usuario.'});
         return res.status(200).send({user: userUpdated});
-    })
+    });
+}
+
+//Subir archivos de imagen/avatar de usuario
+function uploadImage(req, res){
+    var userId = req.params.id;
+    
+    if(req.files){
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+        if(userId != req.user.sub){
+            return removeFilesOfUploads(res, file_path,  'No tienes permiso para actualizar la imagen del usuario.');
+        }
+        if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif'){
+            //Actualizar documento del usuario logueado
+            User.findByIdAndUpdate(userId, {image: file_name}, {new: true}, (err, userUpdated) => {
+                if(err) return res.status(500).send({ message: 'Error en la petición.'});
+                if(!userUpdated) return res.status(404).send({ message: 'No se ha podido actualizar el usuario.'});
+                return res.status(200).send({user: userUpdated});
+            });
+        }else{
+            return removeFilesOfUploads(res, file_path, 'Extensión no valida');
+        }
+    }else{
+        return res.status(200).send({message: 'No se ha subido ningún archivo.'});
+    }
+}
+
+function removeFilesOfUploads(res, file_path, message) {
+    fs.unlink(file_path, (err)  => {
+        return res.status(200).send({message: message});
+    });
+}
+
+//Devolver imagen de usuario
+function getImageFile(req, res){
+    var image_file = req.params.imageFile;
+    var path_file = './uploads/users/' +image_file;
+    fs.exists(path_file, (exists) => {
+        if(exists){
+            res.sendFile(path.resolve(path_file));
+        }else {
+            res.status(200).send({message: 'No existe la imagen'});
+        }
+    });
 }
 
 //Método de prueba
@@ -135,10 +184,12 @@ function pruebas(req, res){
 }
 
 module.exports = {
+    pruebas,
     saveUser, 
     loginUser,
     getUser,
     getUsers,
     updateUser,
-    pruebas
+    uploadImage,
+    getImageFile
 }
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,9 +2,11 @@
 
 var express = require('express');
 var UserController = require('../controllers/user');
+var multipart = require('connect-multiparty');
 
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
+var md_upload = multipart({uploadDir: './uploads/users'});
 
 api.get('/pruebas', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
@@ -12,5 +14,7 @@ api.post('/login', UserController.loginUser);
 api.get('/user/:id', md_auth.ensureAuth, UserController.getUser);
 api.get('/users/:page?', md_auth.ensureAuth, UserController.getUsers);
 api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
+api.post('/upload-image-user/:id', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
+api.get('/get-image-user/:imageFile', UserController.getImageFile);
 
 module.exports = api;
