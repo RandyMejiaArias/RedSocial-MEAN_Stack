f28bbfe8a45710207fb28178ccdc9074c0601dd3
RandyMejiaArias <rdmejia3@espe.edu.ec>
2020-02-12 14:30:26 -0500

    Listar seguidores implementado

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -24,7 +24,7 @@ function saveFollow(req, res){
     follow.save((err, followStored) => {
         if(err) return res.status(500).send({message: 'Error al guardar el seguimiento.'});
         if(!followStored) return res.status(404).send({message: 'El seguimiento no se ha guardado.'});
-        return res.send(200).send({follow: followStored});
+        return res.status(200).send({follow: followStored});
     });
 }
 
@@ -42,11 +42,13 @@ function deleteFollow(req, res) {
 //Lista de usuarios que sigo
 function getFollowingUsers(req, res){
     var userId = req.user.sub;
-    var page = 1;
     
     if(req.params.id && req.params.page){
         userId = req.params.id;
     }
+    
+    var page = 1;
+    
     if(req.params.page){
         page = req.params.params;
     }else{
@@ -67,11 +69,38 @@ function getFollowingUsers(req, res){
 }
 
 //Lista de usuarios que me siguen
+function getFollowedUsers(req, res){
+    var userId = req.user.sub;
+    
+    if(req.params.id && req.params.page){
+        userId = req.params.id;
+    }
+    
+    var page = 1;
+    
+    if(req.params.page){
+        page = req.params.params;
+    }else{
+        page = req.params.id;
+    }
 
+    var itermsPerPage = 4;
+
+    Follow.find({followed: userId}).populate('user followed').paginate(page, itermsPerPage, (err, follows, total) => {
+        if(err) return res.status(500).send({message: 'Error al mostrar los seguidores.'});
+        if(!follows) return res.status(404).send({message: 'Todavía no te sigue ningún usuario.'});
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itermsPerPage),
+            follows
+        });
+    });
+}
 
 module.exports = {
     prueba,
     saveFollow,
     deleteFollow,
-    getFollowingUsers
+    getFollowingUsers,
+    getFollowedUsers
 }
