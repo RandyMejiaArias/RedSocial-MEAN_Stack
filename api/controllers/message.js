24cd02e06719a782babd8a1f1f1e537e5378bef0
RandyMejiaArias <rdmejia3@espe.edu.ec>
2020-02-13 21:52:01 -0500

    Contar mensajes no leídos

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -26,6 +26,7 @@ function saveMessage(req, res){
         message.receiver = params.receiver;
         message.text = params.text;
         message.created_at = moment().unix();
+        message.viewed = 'false';
 
         message.save((err, messageStored) => {
             if(err) return res.status(500).send({message: 'Error en la petición.'});
@@ -72,10 +73,23 @@ function getEmitedMessages(req, res){
     });
 }
 
+//Contar mensajes no leídos
+function getUnviewedMessages(req, res){
+    var userId = req.user.sub;
+
+    Message.count({receiver: userId, viewed: 'false'}).exec((err, count) => {
+        if(err) return res.status(500).send({message: 'Error en la petición.'});
+        return res.status(200).send({
+            'unviewed': count
+        });
+    });
+}
+
 
 module.exports = {
     prueba,
     saveMessage,
     getReceivedMessages,
-    getEmitedMessages
+    getEmitedMessages,
+    getUnviewedMessages
 }
--- api/routes/message.js
+++ api/routes/message.js
@@ -10,5 +10,6 @@ api.get('/prueba-message', md_auth.ensureAuth, MessageController.prueba);
 api.post('/message', md_auth.ensureAuth, MessageController.saveMessage);
 api.get('/my-messages/:page?', md_auth.ensureAuth, MessageController.getReceivedMessages);
 api.get('/messages-sent/:page?', md_auth.ensureAuth, MessageController.getEmitedMessages);
+api.get('/messages-unviewed/:page?', md_auth.ensureAuth, MessageController.getUnviewedMessages);
 
 module.exports = api;
